16f03b1ef8c9e101e80f93ac9552bbb7874cf955
fcampus <fcampus.dev@gmail.com>
2021-01-23 13:26:52 +0100

    Test vue app

---
+++ proporzione/precache-manifest.99836f067139f8c610d55dd7aba00cce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8d065e0ff2a70a8731",
+    "url": "/css/app.a210abb8.css"
+  },
+  {
+    "revision": "9c87164c50434b44eb5cd9c28f948c51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f8d065e0ff2a70a8731",
+    "url": "/js/app.a08f35b3.js"
+  },
+  {
+    "revision": "0959c7ec248e758815db",
+    "url": "/js/chunk-vendors.f9cb04f1.js"
+  },
+  {
+    "revision": "f413f639963582d81ce4ee562653374c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "/robots.txt"
+  }
+]);
